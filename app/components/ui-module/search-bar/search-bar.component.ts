752b6cf5a82515cea3c17866de2cef5c3536cf4f
atellmer <atellmer@gmail.com>
2017-01-09 10:18:29 +0500

    added tabs, search-bar, route-link highliting

---
+++ app/components/ui-module/search-bar/search-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'tm-search-bar',
+  templateUrl: 'search-bar.component.html',
+  styleUrls: ['search-bar.component.css']
+})
+export class SearchBarComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
--- app/components/ui-module/ui.module.ts
+++ app/components/ui-module/ui.module.ts
@@ -6,17 +6,20 @@ import { CommonModule } from '@angular/common';
 // Components
 import { AvatarComponent } from './avatar/avatar.component';
 import { BadgeComponent } from './badge/badge.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 
 @NgModule({
   imports: [CommonModule],
   exports: [
     AvatarComponent,
-    BadgeComponent
+    BadgeComponent,
+    SearchBarComponent
   ],
   declarations: [
     AvatarComponent,
-    BadgeComponent
+    BadgeComponent,
+    SearchBarComponent
   ],
   providers: [],
 })
